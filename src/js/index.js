81b207f4c8253c061c502fcbf53a35ffe334ce57
axmiao <18610834353@163.com>
2017-09-14 16:30:12 +0800

    小调整

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,27 @@ $(function(){
         $('.cen-tit .name').text(intxt);
         $('.show').eq($idx).show().siblings('.show').hide();
     });
+
+	// 移动端menu弹出效果；
+	var isMenuDown = false;
+	$('.menudown').on('click',function(){
+		if(isMenuDown == false){
+			$('.menu-open').addClass('hiden');
+			$('.menu-close').removeClass('hiden');
+			$('.down').removeClass('down').addClass('up');
+			$(".menu-ph").show().removeClass('menuhide').addClass('menushow');
+			isMenuDown = true;
+			return;
+		}else{
+			$('.menu-open').removeClass('hiden');
+			$('.menu-close').addClass('hiden');
+			$('.up').removeClass('up').addClass('down');
+			$('.menu-ph').removeClass('menushow').addClass('menuhide');
+			setTimeout(function(){
+				$('.menu-ph').hide();
+			},300);
+			isMenuDown = false;
+			return;
+		}
+	})
 });
